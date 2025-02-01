4d0246a67b09cb8375b580778371f771bbb9a933
hovseparakelyan <jr.arakelyan@gmail.com>
2025-02-01 23:28:19 +0400

    chore: bump version to 1.2.9 (versionCode 9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.chatnote"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.2.8"
+        versionCode = 9
+        versionName = "1.2.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
