76c489fe27dc890f8556906f8eee21b40f75f3a1
hovseparakelyan <jr.arakelyan@gmail.com>
2025-02-09 17:52:32 +0400

    refactor: Code cleanup & removed unused AndroidManifest files
    - Refactored code for better readability and maintainability
    - Deleted unused `AndroidManifest.xml` files from redundant locations
    - Improved project structure by removing unnecessary configurations

--- feature/direct-notes/directnotes-domain/src/main/java/com/chatnote/directnotesdomain/usecase/UseCases.kt
+++ feature/direct-notes/directnotes-domain/src/main/java/com/chatnote/directnotesdomain/usecase/UseCases.kt
@@ -6,21 +6,26 @@ import com.chatnote.directnotesdomain.model.NoteActionableContent
 import kotlinx.coroutines.flow.Flow
 
 interface GetNotesUseCase {
+
     operator fun invoke(folderId: Long): Flow<List<Note>>
 }
 
 interface AddNoteUseCase {
+
     suspend operator fun invoke(folderId: Long, note: Note): Result<Unit>
 }
 
 interface ObserveFolderUseCase {
+
     operator fun invoke(folderId: Long): Flow<FolderBaseInfo?>
 }
 
 interface ExtractActionableContentUseCase {
+
     operator fun invoke(fullMessage: String): NoteActionableContent
 }
 
 interface DeleteNoteUseCase {
+
     suspend operator fun invoke(noteId: Long): Result<Unit>
 }
--- feature/home-list/homelist-domain/src/main/java/com/chatnote/domain/usecase/UseCases.kt
+++ feature/home-list/homelist-domain/src/main/java/com/chatnote/domain/usecase/UseCases.kt
@@ -5,29 +5,36 @@ import com.chatnote.domain.model.Folder
 import kotlinx.coroutines.flow.Flow
 
 interface GetFoldersUseCase {
+
     operator fun invoke(): Flow<List<Folder>>
 }
 
 interface GetFolderByIdUseCase {
+
     suspend operator fun invoke(folderId: Long): Result<Folder>
 }
 
 interface PinFolderUseCase {
+
     suspend operator fun invoke(folderId: Long): Result<Unit>
 }
 
 interface DeleteFolderUseCase {
+
     suspend operator fun invoke(folderId: Long): Result<Int>
 }
 
 interface UnpinFolderUseCase {
+
     suspend operator fun invoke(folderId: Long): Result<Unit>
 }
 
 interface AddOrUpdateFolderUseCase {
+
     suspend operator fun invoke(folderId: Long?, name: String, iconUri: String): Result<Long>
 }
 
 interface InitializeDefaultFoldersUseCase {
+
     suspend operator fun invoke(defaultFolders: List<DefaultFolder>)
 }
